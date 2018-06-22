326ea403341994ffbc0e6143047f99e5bacedad0
mstmari <michael.landrum.st.marie@gmail.com>
2018-06-22 16:07:36 -0400

    created book form and controller action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,19 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+   binding.pry
+    @book = Book.new(user_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def book_params
+    params.require(:user).permit(:name, :password, :admin)
+  end
+
 end
